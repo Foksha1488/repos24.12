e5db4bc81f43df203174ff8860b470d3c5c73b67
Foksha1488 <maksimfoksha97@gmail.com>
2024-12-24 09:32:32 +0200

    Add files via upload

---
+++ таск-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace таск_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+       
+
+
+        }
+        static void Func()
+        {
+            Random rnd = new Random();
+            for (int x = 1; x <= 10; x++)
+            {
+                int number = rnd.Next(-40,40);
+                Console.WriteLine($"{x}-е число={number}");
+            }
+            }
+    }
+}
